3a1a3dffaa58a04ae888673eec806ccdb4792e6b
kmyers <kmyers2@wisc.edu>
2024-12-13 16:15:30 -0600

    updating file output names

--- site_of_insertion.py
+++ site_of_insertion.py
@@ -6,9 +6,9 @@ and count from bedfile counts. Output is two files:
     one is a table with strand information
     the other is a WIG file for visualization
     
-input is a text file of BAM files, one per line
+input is a text file of BAM files and BED files, separated by a tab, one per line
 
-will parse BAM files, open the Bed and BAM file, find set and parse to make
+will parse BAM and BED files, find set and parse to make
 table out put with strand information and counts for each position and a wig file.
 
 requires Python3 and argparse, os, pysam, re, sys
@@ -71,12 +71,12 @@ def parse( bam, bedFile ):
                 final_count_list.append(f"{each}\t{final_dict1[each]}\t{final_dict2[each]}\n")
                 wig_out.append(f"{each}\t{final_dict1[each]}\n")
     
-    out_table = bam.replace("lessThan25.bam", "_insertion_site_table.txt")
+    out_table = bam.replace(".bam", "_insertion_site_table.txt")
     with open(out_table, 'w') as o:
         for each in final_count_list:
             o.write(each)
     
-    out_wig = bam.replace("lessThan25.bam", "_insertion_site.wig")            
+    out_wig = bam.replace(".bam", "_insertion_site.wig")            
     with open(out_wig, 'w') as o:
         o.write("track type=wiggle_0\nvariableStep chrom=NC_000913.3 span=1\n")
         for each in wig_out:
@@ -85,7 +85,7 @@ def parse( bam, bedFile ):
 def main():
    
     cmdparser = argparse.ArgumentParser(description="Parsing BAM and mpileup for transposon insertion locations.",
-                                        usage='%(prog)s -f <list of BAM files to process> [optional arguments: -d]', prog='site_of_insertion.py'  )                                  
+                                        usage='%(prog)s -f <list of BAM and BED files to process> [optional arguments: -d]', prog='site_of_insertion.py'  )                                  
     cmdparser.add_argument('-f', '--file',  action='store', dest='FILE' , help='list of BAM files to process, one per line.', metavar='')
     cmdparser.add_argument('-d', '--detail',  action='store_true', dest='DETAIL' , help='Print a more detailed description of the program.')
     cmdResults = vars(cmdparser.parse_args())
@@ -98,7 +98,7 @@ def main():
         print("\nScript Name: site_of_insertion.py")
         print("\tThis script was written my Kevin Myers (kmyers2@wisc.edu).")
         print("\nPurpose : Script to parse BAM file and bedfile counts to get site of transposon insertion and count from bedfile counts.")
-        print("\nInput : Text file with list of BAM files, one per line.")
+        print("\nInput : Text file with list of BAM files and associated BED file, separated by a tab, one per line.")
         sys.exit(1)
     
     if cmdResults['FILE'] is not None:
@@ -110,10 +110,11 @@ def main():
     
     with open(BAM_files, 'r') as f:
         for line in f:
-            bam = line.rstrip('\n')
-            bedFile = bam.replace("lessThan25.bam", "_BinSize1_CPM_norm.bed")
+            bam = line.rstrip().split("\t")[0]
+            bedFile = line.rstrip().split("\t")[1]
+            #bedFile = bam.replace("lessThan25.bam", "_BinSize1_CPM_norm.bed")
             
-            print(f"Analyzing {bam} file now...")
+            print(f"Analyzing {bam} and {bedFile} files now\n")
             
             parse(bam, bedFile)
             
